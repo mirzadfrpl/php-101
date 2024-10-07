fdd77cd12b15b9949cfea471735aac6b2b3d0bae
mirzadfrpl <mirzadf123@gmail.com>
2024-10-07 10:14:02 +0700

    baru oop

--- oop.php
+++ oop.php
@@ -1,5 +1,27 @@
 <?php
 
-class Fruit{
-    
+class Animals{
+    public string $Name;
+    public string $Color;
+
+    public function jump(){
+        echo "iam jumping";
+    }
+
+    public function sound(string $type){
+        echo "I'm $type}";
+    }
 }
+
+$dog = new Animals();
+$dog->Name = "Bulldog";
+$dog->Color = "Black";
+
+$cat = new Animals();
+$cat->Name = "Kitty";
+$cat ->Color = "Orange";
+
+
+var_dump($dog);
+echo "\n";
+var_dump($cat);
